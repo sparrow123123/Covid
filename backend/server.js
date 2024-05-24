b6b6a2e19dbe8450f8a049970b5fe5e1028de4ac
sparrow123123 <73479452+sparrow123123@users.noreply.github.com>
2024-05-24 13:12:39 +0530

    change

--- backend/server.js
+++ backend/server.js
@@ -37,8 +37,12 @@ const db = mysql.createConnection({
 
 
 db.connect((err) => {
- if (err) throw err;
- console.log('Connected to database');
+ if (err) {
+    console.log(err);
+ }else{
+
+     console.log('Connected to database');
+ }
 });
 
 
