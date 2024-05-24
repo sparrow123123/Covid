0ddb2b875cf4533081c6efaf14725a2ba1f6b67e
sparrow123123 <73479452+sparrow123123@users.noreply.github.com>
2024-05-24 09:14:30 +0530

    changed

--- backend/server.js
+++ backend/server.js
@@ -21,10 +21,11 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
- host: 'localhost',
+ host: '127.0.0.1',
  user: 'root',
  password: '9943060731',
- database: 'login'
+ database: 'login',
+ port:"3306"
 });
 
 
