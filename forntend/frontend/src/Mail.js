bf8375f3df6f2587853b2764f86ccd9c0f7a322e
sparrow123123 <73479452+sparrow123123@users.noreply.github.com>
2025-02-17 22:16:24 +0530

    mm

--- forntend/frontend/src/Mail.js
+++ forntend/frontend/src/Mail.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 function Mail() {
-  return (
-    <div>Mail</div>
-  )
+  return <div>Mail</div>;
 }
 
-export default Mail
+export default Mail;
--- forntend/frontend/src/signup.js
+++ forntend/frontend/src/signup.js
@@ -1,62 +1,82 @@
-import axios from 'axios';
-import React,{useState} from 'react'
-import{Link} from 'react-router-dom'
-import { useNavigate } from 'react-router-dom'
+import axios from "axios";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Signup() {
-  const[email,setemail]=useState('');
-  const[user,setuser]=useState('');
-  const[pass,setpass]=useState('');
+  const [email, setemail] = useState("");
+  const [user, setuser] = useState("");
+  const [pass, setpass] = useState("");
   let navigate = useNavigate();
-  function handleSubmit(event)
-  {
+  function handleSubmit(event) {
     event.preventDefault();
-    axios.post('https://covid-4-lhxq.onrender.com/signup',{email,user,pass})
-    .then((res)=>{
-      console.log(res);
-      alert('account created')
-      navigate('/')
-
-    
-  }
-    
-    );
-
+    axios
+      .post("https://covid-4-lhxq.onrender.com/signup", { email, user, pass })
+      .then((res) => {
+        console.log(res);
+        alert("account created");
+        navigate("/");
+      });
   }
 
   return (
-    <div className='sign'> 
+    <div className="sign">
       <div className="outlet">
         <form onSubmit={handleSubmit}>
-            <div className='item'>
+          <div className="item">
             <h1>Create Acount</h1>
-            <label htmlFor="emailid"><b>Mail Id</b></label><br/>
-            <input type='email' id='emailid' placeholder='enter email'onChange={e=>setemail(e.target.value)}></input>
+            <label htmlFor="emailid">
+              <b>Mail Id</b>
+            </label>
+            <br />
+            <input
+              type="email"
+              id="emailid"
+              placeholder="enter email"
+              onChange={(e) => setemail(e.target.value)}
+            ></input>
             <br></br>
             <br></br>
-            
 
-            <label htmlFor="username"><b>USERNAME</b>: </label><br />
-            <input type="text" id="username" name="username" placeholder='Username' onChange={e=>setuser(e.target.value)} /><br />
+            <label htmlFor="username">
+              <b>USERNAME</b>:{" "}
+            </label>
+            <br />
+            <input
+              type="text"
+              id="username"
+              name="username"
+              placeholder="Username"
+              onChange={(e) => setuser(e.target.value)}
+            />
+            <br />
             <br></br>
-          
-            <label htmlFor="password"><b>PASSWORD:</b> </label><br />
-            <input type="number" id="password" placeholder='Password' onChange={e=>setpass(e.target.value)}></input>
-            <br></br>
-          <button id='create-btn' >Create Account</button>
-          <button onClick={()=>{
-            navigate('/login')
-          }} id='create-btn' >Login</button>
-            
-            
 
-            
-            </div>
+            <label htmlFor="password">
+              <b>PASSWORD:</b>{" "}
+            </label>
+            <br />
+            <input
+              type="number"
+              id="password"
+              placeholder="Password"
+              onChange={(e) => setpass(e.target.value)}
+            ></input>
+            <br></br>
+            <button id="create-btn">Create Account</button>
+            <button
+              onClick={() => {
+                navigate("/login");
+              }}
+              id="create-btn"
+            >
+              Login
+            </button>
+          </div>
         </form>
       </div>
     </div>
-    
-  )
+  );
 }
 
-export default Signup
+export default Signup;
